fe3ef1d2987b7dd363286e3d517ad3ed28205890
Florian Claux <florian.claux@etu.unilim.fr>
2024-10-23 19:09:02 +0200

    Add Complete Function to tasks

--- app/(tabs)/todolist.tsx
+++ app/(tabs)/todolist.tsx
@@ -5,6 +5,7 @@ export default function TabTwoScreen() {
   const [taskId, setTaskId] = useState(0);
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState<{ key: string, value: string }[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<{ key: string, value: string }[]>([]);
 
   const handleAddTask = () => {
     if (task.trim()) {
@@ -18,6 +19,25 @@ export default function TabTwoScreen() {
     setTasks(tasks.filter((t) => t.key !== taskKey));
   };
 
+  const handleDeleteCompletedTask = (taskKey: string) => {
+    setCompletedTasks(completedTasks.filter((t) => t.key !== taskKey));
+  };
+
+  const handleAddCompletedTask = (taskKey: string) => {
+    const taskToComplete = tasks.find(t => t.key === taskKey);
+    if (taskToComplete) {
+      setCompletedTasks([...completedTasks, { key: taskToComplete.key, value: taskToComplete.value }]);
+    }
+  }
+
+  const handleUncompletedTask = (taskKey: string) => {
+    const taskToUncomplete = completedTasks.find(t => t.key === taskKey);
+    if (taskToUncomplete) {
+      setTasks([...tasks, { key: taskToUncomplete.key, value: taskToUncomplete.value }]);
+      handleDeleteCompletedTask(taskToUncomplete.key);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>To-Do List</Text>
@@ -28,15 +48,35 @@ export default function TabTwoScreen() {
         onChangeText={setTask}
       />
       <Button title="Ajouter" onPress={handleAddTask} color="#00796b" />
+      <Text style={styles.sectionTitle}>Tâches en cours</Text>
       <FlatList
         data={tasks}
         renderItem={({ item }) => (
-          <View>
-            <TouchableOpacity style={styles.taskItem} onPress={() => handleDeleteTask(item.key)}>
-              <Text>{item.key} : {item.value}</Text>
-              <Text style={styles.deleteButton}>❌</Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity onPress={() => {
+            handleAddCompletedTask(item.key);
+            handleDeleteTask(item.key);
+          }}>
+            <View style={styles.taskItem}>
+              <Text>{item.value}</Text>
+              <TouchableOpacity onPress={() => handleDeleteTask(item.key)}>
+                <Text style={styles.deleteButton}>❌</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+      <Text style={styles.sectionTitle}>Tâches terminées</Text>
+      <FlatList
+        data={completedTasks}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleUncompletedTask(item.key)}>
+            <View style={styles.completedTaskItem}>
+              <Text style={styles.completedTaskText}>{item.value}</Text>
+              <TouchableOpacity  onPress={() => handleDeleteCompletedTask(item.key)}>
+                <Text style={styles.deleteButton}>❌</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
         )}
       />
     </View>
@@ -72,9 +112,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
     borderRadius: 5,
   },
+  completedTaskItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 5,
+  },
+  completedTaskText: {
+    color: '#8a8a8a',
+    textDecorationLine: 'line-through',
+  },
   deleteButton: {
     color: '#d32f2f',
-    width: 300,
+    width: 50,
     textAlign: 'right',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#00796b',
   },
 });
